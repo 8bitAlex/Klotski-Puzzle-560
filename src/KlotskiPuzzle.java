efa867b048e14d5a3a10e41d06625f4222e6089a
Alex Salerno <alex.salerno@me.com>
2015-05-05 13:07:33 -0700

    Created isCollision(X,Y,Block).

--- src/KlotskiPuzzle.java
+++ src/KlotskiPuzzle.java
@@ -10,6 +10,8 @@ import java.util.Map;
  */
 
 public class KlotskiPuzzle {
+	static final String EMPTY = "0";
+	
 	String[][] grid;
 	Map<String, Block> blocks = new Hashtable<String, Block>(); 
 	
@@ -22,8 +24,14 @@ public class KlotskiPuzzle {
 		//TODO output solution
 	}
 	
-	public Boolean move(){
-		return isCollision();
+	/*
+	 * @param x - x-coord to move to
+	 * @param y - y-coord to move to
+	 * @param b - Block being moved
+	 */
+	public Boolean move(int x, int y, Block b){
+		//TODO move piece
+		return isCollision(x,y,b);
 	}
 	
 	/*	=================================================
@@ -46,7 +54,31 @@ public class KlotskiPuzzle {
 		blocks.put("J",new Block(2,2,"J"));
 	}
 	
-	private Boolean isCollision(){
+	/*
+	 * @param x - x-coord to move to
+	 * @param y - y-coord to move to
+	 * @param b - Block being moved
+	 */
+	private Boolean isCollision(int x, int y, Block b){
+		if(grid[x][y].equals(EMPTY) || grid[x][y].equals(b.name)){
+			//check for width collision
+			for(int i=1;i<b.width;i++){
+				if(!grid[x+i][y].equals(EMPTY) && !grid[x+i][y].equals(b.name)){
+					return true;
+				}
+			}
+			//check for height collision
+			for(int i=1;i<b.height;i++){
+				if(!grid[x][y+1].equals(EMPTY) && !grid[x][y+1].equals(b.name)){
+					return true;
+				}
+			}
+			return false;
+		}
+		return true;
+	}
+	
+	private Boolean isSolved(){
 		return false;
 	}
 }
