322a8bb12047f95b46e6f506a4bdadb87d1757a2
Alex Salerno <alex.salerno@me.com>
2015-05-05 11:45:37 -0700

    Initial Commit. Generic Eclipse Java project.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * @author Alex Salerno
+ * @author 
+ * @author
+ * @version 0.1
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
