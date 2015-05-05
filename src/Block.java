d5b67dc82272f9d9bcf7d4971a8f7925939a71f5
Alex Salerno <alex.salerno@me.com>
2015-05-05 12:32:47 -0700

    Initial configuration for blocks. Removed X/Y coord from blocks.

--- src/Block.java
+++ src/Block.java
@@ -4,16 +4,14 @@
  */
 public class Block {
 	String name;
-	int x;
-	int y;
+	int area;
 	int height;
 	int width;
 	
-	public Block(int x, int y, int h, int w, String n){
+	public Block(int h, int w, String n){
 		this.name = n;
-		this.x = x;
-		this.y = y;
 		this.height = h;
 		this.width = w;
+		this.area = h * w;
 	}
 }
---
+++ src/KlotskiPuzzle.java
@@ -0,0 +1,49 @@
+import java.util.Hashtable;
+import java.util.Map;
+
+/**
+ * 
+ */
+
+/**
+ * @author hardline
+ *
+ */
+
+public class KlotskiPuzzle {
+	String[][] grid;
+	Map<String, Block> blocks = new Hashtable<String, Block>(); 
+	
+	public KlotskiPuzzle(){
+		initBlocks();
+	}
+	
+	public void solve(){
+		//TODO solve puzzle
+		//TODO output solution
+	}
+	
+	/*	=================================================
+	 * 	Private Functions
+	 *  =================================================
+	 */ 
+	
+	private void initBlocks(){
+		//Initializes block to default sizes
+		//new Block(Height,Weight,Name)
+		blocks.put("A",new Block(1,2,"A"));
+		blocks.put("B",new Block(1,2,"B"));
+		blocks.put("C",new Block(1,2,"C"));
+		blocks.put("D",new Block(1,2,"D"));
+		blocks.put("E",new Block(2,1,"E"));
+		blocks.put("F",new Block(1,1,"F"));
+		blocks.put("G",new Block(1,1,"G"));
+		blocks.put("H",new Block(1,1,"H"));
+		blocks.put("I",new Block(1,1,"I"));
+		blocks.put("J",new Block(2,2,"J"));
+	}
+	
+	private Boolean isCollision(){
+		return false;
+	}
+}
