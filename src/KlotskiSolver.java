9761d2039dbe6efde948374fc7555b234f4a0da7
Alex Salerno <alex.salerno@me.com>
2015-05-06 13:29:49 -0700

    Started breadth first search on all possible grid moves. Save to grids. TODO push solution to  a queue.

--- src/KlotskiSolver.java
+++ src/KlotskiSolver.java
@@ -1,4 +1,6 @@
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 
 /**
@@ -25,18 +27,45 @@ public class KlotskiSolver {
 	
 	public void solve(Boolean verbose){
 		if(verbose) System.out.println("Solving puzzle...");
+		findValidPath(puzzle.getGridCode(),verbose);
 	}
 	
 	private void printSolution(){
 		
 	}
 	
-	private void findValidPath(){
+	//Root is the grids gridCode
+	private void findValidPath(String rootCode, Boolean verbose){
+		Queue<String> grids = new LinkedList<String>();
+		grids.add(rootCode);
+		pastGrid.add(rootCode);
 		
+		while(!grids.isEmpty()){
+			String current = grids.remove();
+			if(isSolved(current)) break;
+			String[] nextGrid = findAllMoves(current);
+			for(String g : nextGrid){
+				if(!pastGrid.contains(g)){
+					if(verbose) System.out.println("Adding grid " + g + " to queue.");
+					grids.add(g);
+					pastGrid.add(g);
+				} else {
+					
+				}
+			}
+		}
 	}
 	
-	private Move[] findAllMoves(KlotskiPuzzle p){
+	private String[] findAllMoves(String gridCode){
+		KlotskiPuzzle p = new KlotskiPuzzle(gridCode);
+		
+		//return next move grid code
 		return null;
+	}
+	
+	private Boolean isSolved(String gridCode){
+		KlotskiPuzzle p = new KlotskiPuzzle(gridCode);
+		return p.isSolved();
 	}
 
 }
