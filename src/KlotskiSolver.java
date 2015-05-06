392fa33f24a7527304a6e9102f8e66e4f26c38d1
Alex Salerno <alex.salerno@me.com>
2015-05-06 10:46:22 -0700

    Added some empty functions for Solver layout.

--- src/KlotskiSolver.java
+++ src/KlotskiSolver.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Alex Salerno
  * @author John Stoddard
@@ -8,11 +11,24 @@
 
 public class KlotskiSolver {
 	
+	Set<String> pastGrid = new HashSet<String>();
+	KlotskiPuzzle puzzle;
+	
 	public KlotskiSolver(KlotskiPuzzle puzzle){
-		
+		this.puzzle = puzzle;
+		pastGrid.add(puzzle.getGridCode());
+		this.puzzle.move(4,1,"F");
 	}
 	
 	public void solve(){
+		solve(false);
+	}
+	
+	public void solve(Boolean verbose){
+		if(verbose) System.out.println("Solving puzzle...");
+	}
+	
+	private void printSolution(){
 		
 	}
 
